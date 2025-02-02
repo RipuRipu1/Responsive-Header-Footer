7cc49527844233f073f5fb7bc012cbee173cdd26
RipuRipu1 <theworrkkers@gmail.com>
2025-02-02 22:26:41 +0700

    Implemented a JavaScript function to close the navigation without clicking the hamburger menu

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ const sideNavbar = document.querySelector('.navbar-anchors');
 document.querySelector('#hamburger-menu').onclick = () => {
     sideNavbar.classList.toggle('active');
 }
+
+const menu = document.querySelector('#hamburger-menu');
+document.addEventListener('click', function(cursor){
+    if (!menu.contains(cursor.target) && !menu.contains(cursor.target)) {
+        sideNavbar.classList.remove('active');
+    }
+})
